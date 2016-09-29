54ab3969339cca5ba1a35b9999fe598e2914f4b8
Borayvor <boray@abv.bg>
2016-09-29 18:12:22 +0300

    add project

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+$(() => {
+    let data = [
+        {
+            name: "JavaScriptUI DOM TeamWork",
+            link: "http://best.telerikacademy.com/projects/28/Backgammon"
+        },
+        {
+            name: "JavaScript Applications TeamWork",
+            link: "http://best.telerikacademy.com/projects/69/Sazerac-Analytics"
+        },
+        {
+            name: "Slice and Dice Course project",
+            link: "http://best.telerikacademy.com/projects/167/Small-cuisine"
+        }
+    ];
+
+    $.get(`/templates/link.handlebars`)
+        .done((templ) => {
+            let template = Handlebars.compile(templ);
+            let html = template(data);
+            $('#content').html(html);
+        });
+});
