ba66db1452506371b9693ca223d68aeda00be11d
Borayvor <Boray@abv.bg>
2017-02-27 18:43:48 +0200

    refactor css

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,30 @@
+/* globals Handlebars */
+
 $(() => {
     let data = [
         {
-            name: "JavaScriptUI DOM TeamWork",
-            link: "http://best.telerikacademy.com/projects/28/Backgammon"
+            name: 'JavaScriptUI DOM TeamWork',
+            link: 'http://best.telerikacademy.com/projects/28/Backgammon'
         },
         {
-            name: "JavaScript Applications TeamWork",
-            link: "http://best.telerikacademy.com/projects/69/Sazerac-Analytics"
+            name: 'JavaScript Applications TeamWork',
+            link: 'http://best.telerikacademy.com/projects/69/Sazerac-Analytics'
         },
         {
-            name: "Slice and Dice Course project",
-            link: "http://best.telerikacademy.com/projects/167/Small-cuisine"
+            name: 'Slice and Dice Course project',
+            link: 'http://best.telerikacademy.com/projects/167/Small-cuisine'
         },
         {
-            name: "ASP.NET MVC Course project",
-            link: "http://best.telerikacademy.com/projects/382/For-free-time"
+            name: 'ASP.NET MVC Course project',
+            link: 'http://best.telerikacademy.com/projects/382/For-free-time'
         },
         {
-            name: "Angular 2 Course project",
-            link: "http://best.telerikacademy.com/projects/446/Adventure-in-ten-photos"
+            name: 'Angular 2 Course project',
+            link: 'http://best.telerikacademy.com/projects/446/Adventure-in-ten-photos'
         }
     ];
 
-    $.get(`./templates/link.handlebars`)
+    $.get('./templates/link.handlebars')
         .done((templ) => {
             let template = Handlebars.compile(templ);
             let html = template(data);
